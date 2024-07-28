e42eaf16063071777155ded8c55c14babf5e955e
Pujo <pujoheadsoft@gmail.com>
2024-07-28 10:16:44 +0900

    wip

--- polysemy-clean-architecture/test/Usecase/DisplayCompletedTodosSpec.hs
+++ polysemy-clean-architecture/test/Usecase/DisplayCompletedTodosSpec.hs
@@ -13,19 +13,16 @@
 module Usecase.DisplayCompletedTodosSpec (spec) where
 
 import Test.Hspec
-import State.TodoState
 import Usecase.DisplayCompletedTodos (execute, execute2)
 import Domain.User (UserId(UserId))
-import Domain.Todo (logics, todo, TodoTitle (TodoTitle), TodoStatus (Completed), Todo (Todo), Todos, Logics (..))
-import Polysemy.State (runState, put, State)
+import Domain.Todo (todo, TodoTitle (TodoTitle), TodoStatus (Completed), Todos, Logics (..))
 import Data.Function ((&))
-import Polysemy (runM, interpret, embed, Member, Sem, Embed)
+import Polysemy (runM, interpret, Sem)
 import Usecase.TodoPort
 import Usecase.TodoOutputPort
 import Domain.Error (Error(Error))
-import Test.HMock (makeMockable, runMockT, ExpectContext (expectAny, expect), (|->))
+import Test.HMock (makeMockable, runMockT, ExpectContext (expect), (|->))
 import Test.MockCat (createStubFn, (|>), createMock, stubFn, shouldApplyTo)
-import Control.Monad.Trans
 
 makeMockable [t|TodoPortClass|]
 makeMockable [t|TodoOutputPortClass|]
@@ -36,30 +33,32 @@ spec = do
     it "execute" $ do
       let
         todos = [todo (TodoTitle "hoge") Completed]
-        completedTodos = [todo (TodoTitle "hoge") Completed]
-      findFn <- createStubFn $ todos |> completedTodos
-
-      xx <- createStubFn $ UserId 10 |> (pure (Right todos) :: Sem [TodoOutputPort, Embed IO] (Either Error Todos))
-      yy <- createMock $ completedTodos |> (pure () :: Sem '[Embed IO] ())
+        completedTodos = [] :: Todos
+      f <- createStubFn $ todos |> completedTodos
+      findTodosStub <- createStubFn $ UserId 10 |> pure @(Either Error) todos
+      setTodosMock <- createMock $ completedTodos |> ()
       
       let
-        yf = stubFn yy
-        --runTodoGateway :: Sem (TodoPort : r) a -> Sem r a
+        runTodoGateway :: Sem (TodoPort : r) a -> Sem r a
         runTodoGateway = interpret $ \case
-          FindTodos userId -> xx userId
+          FindTodos userId -> pure $ findTodosStub userId
 
-        --runOutputPort :: Member (State TodoState) r => Sem (TodoOutputPort : r) a -> Sem r a
+        runOutputPort :: Sem (TodoOutputPort : r) a -> Sem r a
         runOutputPort = interpret $ \case
-          SetTodos t -> yf t
+          SetTodos t -> pure $ stubFn setTodosMock t
           SetError _ -> undefined
 
-      v <- execute (UserId 10) logics
+        l = Logics {
+          completed = f
+        }
+
+      v <- execute (UserId 10) l
         & runTodoGateway
         & runOutputPort
         & runM
 
       v `shouldBe` ()
-      yy `shouldApplyTo` completedTodos
+      setTodosMock `shouldApplyTo` completedTodos
 
     it "execute2 (experiment)" $ do
       let
@@ -73,28 +72,3 @@ spec = do
           execute2 (UserId 10) Logics {
             completed = f
           }
-
-{-
-    it "" $ do
-      let
-        f = mock $ do
-          "a" :> "b" :> true
-          "b" :> "a" :> false 
-
-        completed_fn = mock $ do
-          [(todo (TodoTitle "hoge") Completed)] :> [(todo (TodoTitle "hoge") Completed)]
-      l = Logics {
-        completed = completed_fn
-      }
-
-      runMockT $ do
-        stub findTodos2 (UserId 10) :> Right [todo (TodoTitle "hoge") Completed]
-        stub any :> ()
-
-        execute2 (UserId 10) l
-
-        verify setTodos2 [todo (TodoTitle "hoge") Completed]
-
-        
--}
-
