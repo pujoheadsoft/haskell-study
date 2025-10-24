2b8ed6066b4056e83ba906715489082b60be41ad
Pujo <pujoheadsoft@gmail.com>
2025-10-24 17:17:35 +0900

    wip

--- cli-using-rest-api-w-effect/src/Application/Port.hs
+++ cli-using-rest-api-w-effect/src/Application/Port.hs
@@ -16,20 +16,6 @@ data OutputPort m a where
   SavePostWithCommentsList :: FilePath -> [PostWithComments] -> OutputPort m (Either AppError ())
 makeSem ''OutputPort
 
-
 data Logger m a where
   LogInfo :: Text -> Logger m ()
 makeSem ''Logger
-
--- class Monad m => UserDataPort m where
---   getPosts :: UserId -> m (Either AppError [Post])
---   getPostWithComments :: Post -> m (Either AppError PostWithComments)
-
--- class Monad m => OutputPort m where
---   savePostWithCommentsList :: FilePath -> [PostWithComments] -> m (Either AppError ())
-
--- class Monad m => MonadAsync m where
---   mapConcurrently :: Traversable t => (a -> m b) -> t a -> m (t b)
-
--- class Monad m => Logger m where
---   logInfo :: Text -> m ()
